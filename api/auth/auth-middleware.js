f1e21437f3c03dce851b5ef384363e4af101c8a6
LadyDementor <125830907+LadyDementor@users.noreply.github.com>
2023-06-05 00:41:45 +0300

    json web tokenları kullanma

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,7 @@
 const { JWT_SECRET } = require("../secrets"); // bu secreti kullanın!
+const userModel = require("../users/users-model");
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcryptjs");
 
 const sinirli = (req, res, next) => {
   /*
@@ -7,18 +10,33 @@ const sinirli = (req, res, next) => {
     {
       "message": "Token gereklidir"
     }
-
     Eğer token doğrulanamıyorsa:
     status: 401
     {
       "message": "Token gecersizdir"
     }
-
     Alt akıştaki middlewarelar için hayatı kolaylaştırmak için kodu çözülmüş tokeni req nesnesine koyun!
   */
-}
+  try {
+    let authHeader = req.headers["authorization"]; // req.headers.authorization
+    if (!authHeader) {
+      res.status(401).json({ message: "Token gereklidir" });
+    } else {
+      jwt.verify(authHeader, JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ message: "token gecersizdir" });
+        } else {
+          req.decodedToken = decodedToken;
+          next();
+        }
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-const sadece = role_name => (req, res, next) => {
+const sadece = (role_name) => (req, res, next) => {
   /*
     
 	Kullanıcı, Authorization headerında, kendi payloadu içinde bu fonksiyona bağımsız değişken olarak iletilen 
@@ -27,13 +45,20 @@ const sadece = role_name => (req, res, next) => {
     {
       "message": "Bu, senin için değil"
     }
-
     Tekrar authorize etmekten kaçınmak için kodu çözülmüş tokeni req nesnesinden çekin!
   */
-}
+  try {
+    if (req.decodedToken.role_name === role_name) {
+      next();
+    } else {
+      res.status(403).json({ message: "Bu, senin için değil" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-
-const usernameVarmi = (req, res, next) => {
+const usernameVarmi = async (req, res, next) => {
   /*
     req.body de verilen username veritabanında yoksa
     status: 401
@@ -41,33 +66,88 @@ const usernameVarmi = (req, res, next) => {
       "message": "Geçersiz kriter"
     }
   */
-}
-
+  try {
+    let isExist = await userModel.goreBul(req.body.username);
+    if (isExist && isExist.length > 0) {
+      let currentUser = isExist[0];
+      let isPasswordMatch = bcryptjs.compareSync(
+        req.body.password,
+        currentUser.password
+      );
+      if (!isPasswordMatch) {
+        res.status(401).json({
+          message: "Geçersiz kriter",
+        });
+      } else {
+        req.currentUser = currentUser;
+        next();
+      }
+    } else {
+      res.status(401).json({
+        message: "Geçersiz kriter",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const rolAdiGecerlimi = (req, res, next) => {
   /*
     Bodydeki role_name geçerliyse, req.role_name öğesini trimleyin ve devam edin.
-
     Req.body'de role_name eksikse veya trimden sonra sadece boş bir string kaldıysa,
     req.role_name öğesini "student" olarak ayarlayın ve isteğin devam etmesine izin verin.
-
     Stringi trimledikten sonra kalan role_name 'admin' ise:
     status: 422
     {
       "message": "Rol adı admin olamaz"
     }
-
     Trimden sonra rol adı 32 karakterden fazlaysa:
     status: 422
     {
       "message": "rol adı 32 karakterden fazla olamaz"
     }
   */
-}
+  try {
+    let { role_name } = req.body;
+    if (!role_name) {
+      req.body.role_name = "student";
+      next();
+    } else {
+      role_name = role_name.trim();
+      if (role_name.length > 32) {
+        res
+          .status(422)
+          .json({ message: "rol adı 32 karakterden fazla olamaz" });
+      } else if (role_name == "admin") {
+        res.status(422).json({ message: "Rol adı admin olamaz" });
+      } else {
+        req.body.role_name = role_name;
+        next();
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+const checkPayload = (req, res, next) => {
+  try {
+    let { username, password } = req.body;
+    if (!username || !password) {
+      res.status(400).json({ messsage: "Eksik alan var" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
   sinirli,
   usernameVarmi,
   rolAdiGecerlimi,
   sadece,
-}
+  checkPayload,
+};
